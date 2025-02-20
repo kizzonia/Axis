b3d13d7635bdd5b7cbc2010c8e108c8609c05f4d
kizzonia <chibuikefortune@hotmail.com>
2025-02-20 21:20:14 +0100

    second push menu ui and base

--- app/admin/headers.rb
+++ app/admin/headers.rb
@@ -10,8 +10,7 @@ ActiveAdmin.register Header do
   :services_title, :services_sub_title, :boards_title,
   :boards_sub_title, :features_title, :features_sub_title,
   :feedbacks_title, :feedbacks_sub_title, :blogs_title,
-  :blogs_sub_title, :cta1, :cta2, :headerimg, :products_title,
-   :products_sub_title
+  :blogs_sub_title, :cta1, :cta2, :headerimg
   #
   # or
   #
