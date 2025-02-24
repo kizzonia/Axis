afbb9cb09ad0a26a33a9acf541312f0e7f90bd27
kizzonia <chibuikefortune@hotmail.com>
2025-02-24 11:33:40 +0100

    second push menu ui and base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_19_165451) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_20_210751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -161,6 +161,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_19_165451) do
     t.index ["slug"], name: "index_services_on_slug"
   end
 
+  create_table "supports", force: :cascade do |t|
+    t.string "title"
+    t.string "sub_title"
+    t.text "body"
+    t.bigint "help_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["help_id"], name: "index_supports_on_help_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -169,6 +179,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_19_165451) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "balance"
+    t.boolean "status"
+    t.boolean "verified"
+    t.text "body"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -194,4 +210,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_19_165451) do
   end
 
   add_foreign_key "accounts", "users"
+  add_foreign_key "supports", "helps"
 end
