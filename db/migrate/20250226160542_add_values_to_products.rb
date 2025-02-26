adb245481c330d9321658acf7f559e652a4b9993
kizzonia <chibuikefortune@hotmail.com>
2025-02-26 17:38:48 +0100

    Products Model

--- db/migrate/20250226160542_add_values_to_products.rb
+++ db/migrate/20250226160542_add_values_to_products.rb
@@ -2,7 +2,7 @@ class AddValuesToProducts < ActiveRecord::Migration[7.2]
   def change
     add_column :products, :sku, :string
     add_column :products, :state, :string
-    add_column :products, :status, :boolean
+    add_column :products, :featured, :boolean
     add_column :products, :discounttype, :string
   end
 end
