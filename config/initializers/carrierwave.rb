e3f52094d6620deb24c58bb4957d2f241f651b80
kizzonia <chibuikefortune@hotmail.com>
2025-03-01 02:39:32 +0100

    subcategories

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
-if Rails.env.production?
-require 'carrierwave/storage/fog'
-# config/initializers/carrierwave.rb
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws' # Use 'fog-aws' if you're using the fog-aws gem
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                ENV['AWS_REGION']
-  }
-  config.fog_directory  = ENV['AWS_BUCKET']
-  config.fog_public     = false
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-end
-end
+# if Rails.env.production?
+# require 'carrierwave/storage/fog'
+# # config/initializers/carrierwave.rb
+# CarrierWave.configure do |config|
+#   config.fog_provider = 'fog/aws' # Use 'fog-aws' if you're using the fog-aws gem
+#   config.fog_credentials = {
+#     provider:              'AWS',
+#     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+#     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#     region:                ENV['AWS_REGION']
+#   }
+#   config.fog_directory  = ENV['AWS_BUCKET']
+#   config.fog_public     = false
+#   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+# end
+# end
 
 # if Rails.env.production?
 # require 'carrierwave/storage/fog'
@@ -39,3 +39,22 @@ end
 #   # co
 # end
 # end
+if Rails.env.production?
+require 'carrierwave/storage/fog'
+CarrierWave.configure do |config|
+  config.storage = :fog
+ config.fog_provider = 'fog/aws'
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
+    :region                 => 'us-east-2',                  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                             # required
+  config.fog_public     = true                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
+end
