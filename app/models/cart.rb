15d772d64831bccf2aa2c87373147f4e0cde7da4
kizzonia <chibuikefortune@hotmail.com>
2025-03-08 23:28:31 +0100

    Cart Products Show

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,12 @@ class Cart < ApplicationRecord
   belongs_to :user
  has_many :cart_items
  has_many :products, through: :cart_items
+
+
+
+   # Calculate the total price of all items in the cart
+   def total_price
+     cart_items.sum { |cart_item| cart_item.quantity * cart_item.product.price }
+   end
+
 end
