35eb4e88979b7e9ab362a253c3ed5d6c1aed710b
kizzonia <chibuikefortune@hotmail.com>
2025-03-13 23:13:09 +0100

    products feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # mount Ckeditor::Engine => '/ckeditor'
+  mount Ckeditor::Engine => '/ckeditor'
   get "carts/show"
 
   resources :cart_items, only: [:create, :destroy]
