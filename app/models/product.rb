c81f32245db90a5bd5af6286890d28070456d5e3
kizzonia <chibuikefortune@hotmail.com>
2025-03-16 18:42:02 +0100

    products feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :name, use: :slugged
   mount_uploader :prodimage, ProdimageUploader
 
-  # has_rich_text :description
+  has_rich_text :description
   belongs_to :category
   belongs_to :sub_category
   belongs_to :user
