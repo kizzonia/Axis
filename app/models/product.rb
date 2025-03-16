80a9bff03316c2bbe023bb287bb2ce07ac6b08a0
kizzonia <chibuikefortune@hotmail.com>
2025-03-16 18:01:49 +0100

    products feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,14 +3,7 @@ class Product < ApplicationRecord
   friendly_id :title, use: :slugged
   mount_uploader :prodimage, ProdimageUploader
 
-  def self.ransackable_attributes(auth_object = nil)
-    authorizable_ransackable_attributes
-  end
-
-  def self.ransackable_associations(auth_object = nil)
-    authorizable_ransackable_associations
-  end
-  has_rich_text :description
+  # has_rich_text :description
   belongs_to :category
   belongs_to :sub_category
   belongs_to :user
