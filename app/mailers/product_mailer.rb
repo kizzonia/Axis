4b1d89f99e5f53042ee475e4468f6a318d2284a6
kizzonia <chibuikefortune@hotmail.com>
2025-03-22 06:40:25 +0100

    products feature

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -1,6 +1,6 @@
 class ProductMailer < ApplicationMailer
   default :from => "support@theaxismarket.com"
-  def account_email(user, product)
+  def product_email(user, product)
     @user = user
     @product = product
      mail(to: user.email, subject: 'Product Upload')
