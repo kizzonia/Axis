5106474ead880a365233ed5a219415fd0c9eeb4e
kizzonia <chibuikefortune@hotmail.com>
2025-04-05 10:32:12 +0100

    transactions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,75 +2,69 @@ class ProductsController < InheritedResources::Base
   layout :resolve_layout
   before_action :authenticate_user!, except: [:show]
   add_flash_types :success, :danger, :info, :notice
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
-  before_action :find_product, only: [ :show, :edit, :update, :destroy]
-
-
-def index
+  def index
     @accounts = Account.where(user_id: current_user).order('created_at ASC')
-    @products = Product.where(user_id: current_user).order('created_at ASC')
-    @products = current_user.products.includes(:category, :sub_category)
+    @products = current_user.products.includes(:category, :sub_category, :images).order('created_at ASC')
     @wallets = Wallet.where(user_id: current_user).order('created_at ASC')
+  end
 
-end
+  def show
+    @wallets = Wallet.where(user_id: current_user).order('created_at ASC')
+    @headers = Header.all
+    @blogs = Blog.all.order('created_at DESC')
+    @feedbacks = Feedback.all.order('created_at DESC')
+    @categories = Category.includes(:sub_categories).all
+  end
 
-def show
-  @wallets = Wallet.where(user_id: current_user).order('created_at ASC')
+  def new
+    @categories = Category.includes(:sub_categories).all
+    @product = current_user.products.new
+    @product.images.build # Initialize empty image for the form
+  end
 
-  @headers = Header.all
-  @blogs = Blog.all.order('created_at DESC')
-  @feedbacks = Feedback.all.order('created_at DESC')
-  @categories = Category.includes(:sub_categories).all
+  def create
+    @product = current_user.products.new(product_params)
 
-end
-   def new
-     @categories = Category.includes(:sub_categories).all
-     @product = current_user.products.new
-   end
-
-   def create
-     @product = current_user.products.new(product_params)
-     if @product.save
-       user = User.find_by_id(@product.user_id)
+    if @product.save
+      user = User.find_by_id(@product.user_id)
       product = @product
-       ProductMailer.product_email(user, product).deliver_later
+      ProductMailer.product_email(user, product).deliver_later
 
-       redirect_to products_path, notice: 'Product was successfully created. An email has been sent to you with your product details '
-     else
-       render :new
-     end
-   end
+      redirect_to products_path, notice: 'Product was successfully created. An email has been sent to you with your product details.'
+    else
+      @product.images.build if @product.images.empty? # Ensure at least one image field on validation error
+      render :new
+    end
+  end
 
-   def edit
-   end
+  def edit
+    @categories = Category.includes(:sub_categories).all
+    @product.images.build if @product.images.empty? # Add empty image field if none exist
+  end
 
-   def update
-     # @product = current_user.products.find(params[:id])
-     # if @product.update(product_params)
-     #   redirect_to products_path, notice: 'Product was successfully updated.'
-     # else
-     #   render :edit
-     # end
+  def update
+    if @product.update(product_params)
+      redirect_to products_path, notice: 'Product was successfully updated.'
+    else
+      @product.images.build if @product.images.empty? # Ensure image field on validation error
+      render :edit
+    end
+  end
 
-     if @product.update(product_params)
-       redirect_to root_path
-     else
-       render 'edit'
-     end
-   end
-
-   def destroy
-     @product = current_user.products.find(params[:id])
-     @product.destroy
-     redirect_to products_path, notice: 'Product was successfully deleted.'
-   end
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: 'Product was successfully deleted.'
+  end
 
   private
+
   def resolve_layout
     case action_name
     when "new", "create", "edit", "index"
       "accounts"
-    when  "show"
+    when "show"
       "application"
     else
       "application"
@@ -81,8 +75,12 @@ end
     @product = Product.friendly.find(params[:id])
   end
 
-    def product_params
-      params.require(:product).permit(:name, :title, :description, :sku, :slug, :overview, :verified, :state, :feature, :discounttype, :price, :prodimage, :status, :category_id, :sub_category_id, :user_id)
-    end
-
+  def product_params
+    params.require(:product).permit(
+      :name, :title, :description, :sku, :slug, :overview,
+      :verified, :state, :feature, :discounttype, :price,
+      :prodimage, :status, :category_id, :sub_category_id, :user_id,
+      images_attributes: [:id, :image, :_destroy] # Permit nested image attributes
+    )
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
   mount_uploader :prodimage, ProdimageUploader
-
+  has_many :images, dependent: :destroy
+   accepts_nested_attributes_for :images, allow_destroy: true
+   
  has_rich_text :description
   belongs_to :category
   belongs_to :sub_category
